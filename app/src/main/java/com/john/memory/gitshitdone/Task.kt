97678f89afe069dd0c741a410984a56c0588217d
Alex Morgan <axemorgan@gmail.com>
2018-10-04 21:23:12 -0400

    Prep for RecyclerView

---
+++ app/src/main/java/com/john/memory/gitshitdone/Task.kt
@@ -0,0 +1,5 @@
+package com.john.memory.gitshitdone
+
+data class Task() {
+    
+}
---
+++ app/src/main/java/com/john/memory/gitshitdone/TaskAdapter.kt
@@ -0,0 +1,10 @@
+package com.john.memory.gitshitdone
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class TaskAdapter : RecyclerView.Adapter<TaskViewHolder>() {
+
+}
+
+class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
