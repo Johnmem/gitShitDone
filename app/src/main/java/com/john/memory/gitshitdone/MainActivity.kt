9ab711bcb25fe4dd3e784cc7e0e15da45fb81c06
Memory <212628619@logon.ds.ge.com>
2018-10-04 20:49:52 -0400

    Init commit

---
+++ app/src/main/java/com/john/memory/gitshitdone/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.john.memory.gitshitdone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.supportActionBar?.title = "Task List"
+
+        bSavin.setOnClickListener {
+            var taskName :String = inputBox.text.toString()
+            Log.i("task", taskName.toString())
+            Toast.makeText( this, taskName.toString(),Toast.LENGTH_LONG).show()
+            var taskList :String = TaskBox.text.toString()
+            TaskBox.text = taskList + '\n' + taskName
+            inputBox.setText("")
+        }
+
+
+    }
+    //function to take a list of tasks and return them as a string
+    //fun createTaskString (taskList: List) {}
+}
